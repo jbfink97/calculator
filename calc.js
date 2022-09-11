877bbf5c7feb19246ea6835577190788f1d1c981
Johnny <johnnyfink97@gmail.com>
2022-09-10 23:44:25 -0400

    created functions and constants

---
+++ calc.js
@@ -0,0 +1,73 @@
+window.addEventListener('load', () => {
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function subtract(a,b) {
+        return a - b;
+    }
+
+    function multiply(a,b) {
+        return a * b;
+    }
+
+    function divide(a,b) {
+        return a / b;
+    }
+
+    function operate(operator, num1, num2) {
+        if (operator == '+') {
+            return Math.round(add(num1, num2) *1000) / 1000;
+        }
+        if (operator == '-') {
+            return Math.round(subtract(num1, num2) *1000) / 1000;
+        }
+        if (operator == 'x') {
+            return Math.round(multiply(num1, num2) *1000) / 1000;
+        }
+        if (operator == '/') {
+            if (num2 == 0) {
+                return display.textContent = 'no';
+            }
+            return Math.round(divide(num1, num2) *1000) / 1000;
+        }
+    }
+
+    function percent(num) {
+        return number / 100;
+    }
+
+    function updateDisplayValue() {
+        displayValue = Number(display.textContent);
+    }
+
+    function removeHighlight(){
+        mathBtns.forEach(btn => {
+            btn.classList.remove('pushed');
+        })
+    }
+
+    const display = document.querySelector('#display');
+    const numberBtns = document.querySelectorAll('.number');
+    const clearBtn = document.querySelector('.clear');
+    const mathBtns = document.querySelectorAll('.math');
+    const percentBtn = document.querySelector('.percent');
+    const negativeBtn = document.querySelector('.negative');
+    const equalBtn = document.querySelector('.equal');
+    let displayValue = Number(display.textContent);
+    let num1;
+    let num2;
+    let operator;
+    let operatorButtonPushed = false;
+    let count = 0;
+    let disabledDecimal = false;
+
+
+
+
+
+
+
+
+})
